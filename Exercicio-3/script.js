23199956b12533426b7412e74bec9f8371204631
Vivian-Oliveira <costa.vivs@gmail.com>
2020-12-22 19:47:59 +0000

    criei programa com 2 jogadores, quem escolher o menos numero vence

--- Exercicio-3/script.js
+++ Exercicio-3/script.js
@@ -1,12 +1,24 @@
 //Faca um programa que peca para o usuario digitar 2 numeros, e mostre um booleam indicando se a media for maior que 5
 
-const numero1 = Number(prompt('Digite o primeiro numero: '));
-const numero2 = Number(prompt('Digite o segundo numero: '));
+// const numero1 = Number(prompt('Digite o primeiro numero: '));
+// const numero2 = Number(prompt('Digite o segundo numero: '));
 
-const resultado = (numero1 + numero2) / 2;
+// const resultado = (numero1 + numero2) / 2;
 
-if(resultado > 5){
-    alert(true)
-} else {
-    alert(false)
-}
+// if(resultado > 5){
+//     alert(true)
+// } else {
+//     alert(false)
+// }
+
+//Faca um programa que peca para o usuario digitar 2 numeros, e mostre uma mensagem se ele foi aprovado ou nao, a nota de corte eh 5.
+
+// const nota1 = Number(prompt('Digite a primeira nota: '));
+// const nota2 = Number(prompt('Digite a segunda nota: '));
+// const media = (nota1 + nota2) / 2;
+
+// if(media >= 5){
+//     alert('Parabens, voce foi aprovado!')
+// } else {
+//     alert('Que pena, voce foi reprovado!')
+// }
---
+++ Exercicio-3/script2.js
@@ -0,0 +1,21 @@
+/* Fazer jogador 1 digitar um numero, fazer jogador 2 digitar outro numero. Se os numeros forem iguais, mostrar a mensagem "empate" e
+encerrar o programa. Se forem diferentes, sortear um numero entre 0 e 1, se o numero sorteado for 0, ganha quem escolher o menor numero,
+se nao, ganha quem escolher o maior numero. */
+
+
+alert('Jogador 1')
+const player1 = Number(prompt('Digite um numero'));
+alert('Jogador 2')
+const player2 = Number(prompt('Digite um numero'));
+const sorteio = Number(Math.random() * 2);
+
+if(player1 === player2){
+    alert('Empatou!')
+} 
+else if (sorteio === 0){
+}
+else if(player1 < player2){
+    alert('Jogador 1 ganhou!')
+} else {
+    alert('Jogador 2 ganhou!')
+}
