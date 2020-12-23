aaaac75c5cbb66e37f0dc86de9f05e18607cf870
Vivian-Oliveira <costa.vivs@gmail.com>
2020-12-23 17:34:10 +0000

    Criei programa que recebe um numero de um usuario e mostra a tabuada desse numero de 1 a 1000 com uma linha divisoria a cada dezena

--- Exercicio-6/script.js
+++ Exercicio-6/script.js
@@ -3,13 +3,8 @@
 const tabuadaNumero = Number(prompt('Digite um numero: '));
 let indice = 1;
 
-// for(let i = 0; i <= 10; i++){
-//     console.log(`${tabuadaNumero} x ${indice} = ${tabuadaNumero * indice}`)
-//     i ++;   
-// }
-
 while(indice <= 1000) {
     console.log(`${tabuadaNumero} x ${indice} = ${tabuadaNumero * indice}`);
-    indice = indice + 1;
+    indice ++;
 }
 console.log('Saiu do loop');
